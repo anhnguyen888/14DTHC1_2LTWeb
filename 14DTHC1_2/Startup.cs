f20b40a99de3df9b39c87d8354155bb1fb0b5ee8
dinhanh <nd.anh@hutech.edu.vn>
2017-09-18 06:56:44 +0700

    Add project files.

---
+++ 14DTHC1_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_14DTHC1_2.Startup))]
+namespace _14DTHC1_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
